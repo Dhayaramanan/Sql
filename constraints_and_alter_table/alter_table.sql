6dd8891fec9fed9ebb7e58483eaba48ea7182c98
Dhaya <dhayagithub@gmail.com>
2024-06-20 18:38:16 +0530

    learned few methods of altering table with ALTER TABLE

--- constraints_and_alter_table/alter_table.sql
+++ constraints_and_alter_table/alter_table.sql
@@ -13,3 +13,14 @@ RENAME COLUMN name TO company_name;
 
 ALTER TABLE companies
 MODIFY company_name VARCHAR(100);
+
+ALTER TABLE companies
+MODIFY company_name VARCHAR(100) DEFAULT 'unknown';
+
+-- rename column and change its definition
+alter table suppliers
+change companies biz_name varchar(200);
+
+-- constraints
+alter table houses add constraints positive_price check (purchase_price >= 0);
+alter table houses drop constraints positive_price;
